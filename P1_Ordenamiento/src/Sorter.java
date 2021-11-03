ab65719f3ff11c00c5c8486d560004cdf6dcf007
FabriK12 <fbalarezo@unsa.edu.pe>
2021-11-02 23:07:17 -0500

    Se completo el insertion sort, falta comentar algunas lineas

---
+++ P1_Ordenamiento/src/Sorter.java
@@ -0,0 +1,26 @@
+public class Sorter {
+    public static void HeapSort(Object[] array) {
+
+    }
+    public static void QuickSort(Object[] array) {
+
+    }
+
+    /**
+     * @param arr que es un arreglo de objetos
+     *
+     * @return un arreglo de objetos ordenado
+     */
+    public static <E extends Comparable<E>> void InsertionSort(E[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            E key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && key.compareTo(arr[j]) < 0){
+                arr[j + 1] = arr[j];
+                j -= 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
